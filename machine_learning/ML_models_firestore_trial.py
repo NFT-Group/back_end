4f5281e8ceab7faf75fd32b036bb2b319fc7b53e
Tom Mitcheson <tm21@sprite16.doc.ic.ac.uk>
2022-02-24 19:44:29 +0000

    retrieving from the database now works and we can use multiple databases within the same python file. time to see if we can write to a different one to that which we pulled from

--- machine_learning/ML_models_firestore_trial.py
+++ machine_learning/ML_models_firestore_trial.py
@@ -4,27 +4,48 @@ from firebase_admin import db
 import numpy as np
 from trait_distribution import trait_distribution 
 import json
+import pathlib
 
-cred = firebase_admin.credentials.Certificate("/homes/tm21/Documents/key_for_all_tokens_store.json")
-default_app = firebase_admin.initialize_app(cred, {
-    'databaseURL':'https://alltokens-8ff48-default-rtdb.europe-west1.firebasedatabase.app/'
+# CREATE LINK FOR 'FULL DATABASE'
+
+cred_push_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_full_database_store.json'
+cred_push = firebase_admin.credentials.Certificate(cred_push_key)
+default_app = firebase_admin.initialize_app(cred_push, {
+    'databaseURL':'https://full-database-9c028-default-rtdb.europe-west1.firebasedatabase.app/'
     })
 
-ref = db.reference('/')
+# CREATE LINK FOR 'ALL TOKENS' DATABASE
 
-bored_apes = ref.order_by_key().start_at('boredape').limit_to_first(5).get()
+cred_pull_tokens_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_all_tokens_store.json'
+cred_pull_tokens = firebase_admin.credentials.Certificate(cred_pull_tokens_key)
+tokens_app = firebase_admin.initialize_app(cred_pull_tokens, {
+    'databaseURL':'https://alltokens-8ff48-default-rtdb.europe-west1.firebasedatabase.app/'
+    }, name = 'tokens_app')
+
+# CREATE LINK FOR 'ALL TRANSACTIONS' DATABASE
+
+cred_pull_transactions_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_all_transactions_store.json'
+cred_pull_transactions = firebase_admin.credentials.Certificate(cred_pull_transactions_key)
+transactions_app = firebase_admin.initialize_app(cred_pull_transactions, {
+    'databaseURL':'https://allcollections-6e66c-default-rtdb.europe-west1.firebasedatabase.app/'
+    }, name='transactions_app')
+
+# ref = tokens_app.reference('/')
+ref = db.reference('/', app=tokens_app)
+
+bored_apes = ref.order_by_key().start_at('boredape').end_at('boredapekennel').get()
 # bored_apes_list = list(bored_apes.items())
 
 id_list = []
 metadata_list = []
 
 for id, meta in bored_apes.items():
-    id_list.append(id)
+    id_temp = json.dumps(meta["tokenid"])
+    id_list.append(id_temp)
     metadata_temp = json.dumps(meta["metadata"])
     metadata_temp = metadata_temp.replace("\\","")[1:-1]
     metadata_temp = json.loads(metadata_temp)
     metadata_temp = json.dumps(metadata_temp["attributes"])
-    print(metadata_temp)
     metadata_list.append(metadata_temp)   
 
 print(metadata_list)
@@ -36,4 +57,6 @@ print(trait_values_distribution)
 
 # print(unique_header_list)
 # print(trait_values_distribution)
+
+
 
