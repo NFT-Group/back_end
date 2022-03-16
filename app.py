372390c4dbb04c2bc8a3744a99861942a546dc11
apb121 <99488751+apb121@users.noreply.github.com>
2022-03-16 13:31:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/")
+CORS(ap)
+
+@app.route("/", methods=["POST"])
 def index():
     return "Hello from the other heroku!"
