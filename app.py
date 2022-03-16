de4742b1aa54cfd55e727cc9d2e203e6e6b7449c
apb121 <99488751+apb121@users.noreply.github.com>
2022-03-16 13:45:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+import fs
+import sklearn
+import pandas
 
 app = Flask(__name__)
 
@@ -7,4 +10,8 @@ CORS(app)
 
 @app.route("/", methods=["POST"])
 def index():
-    return "Hello from the other heroku!"
+    response = ""
+    with open("hello.txt") as file:
+        for line in file:
+            response += line
+    return response
