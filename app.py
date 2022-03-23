0d3ad219611dbab0aa6318986b70c7d621904f12
Tom Mitcheson <tm21@curve10.doc.ic.ac.uk>
2022-03-23 14:30:22 +0000

    Added a get results from model file, and added some imports to app.py to see if it breaks

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask_cors import CORS
 import sklearn
 import pandas
 import pickle
+import firebase_admin
+from firebase_admin import credentials, firestore, db
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestRegressor
+
+import pickle
 import os.path
 
 app = Flask(__name__)
