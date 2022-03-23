a0b7706ee4f6926d1d42addaaa53320522b8dc96
Tom Mitcheson <tm21@curve10.doc.ic.ac.uk>
2022-03-23 13:53:12 +0000

    Trying to piece together pipeline

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 import sklearn
 import pandas
 import pickle
 import os.path
+import json
 
 app = Flask(__name__)
 
@@ -12,12 +13,14 @@ CORS(app)
 @app.route("/", methods=["GET", "POST"])
 def index():
     response = ""
+    collection = json.loads(str(request.data))["collection"]
+    response += collection
     with open("hello.txt") as file:
         for line in file:
             response += line
     response += ". "
     if (os.path.exists("boredape_model.pkl")):
-        response += "Bored ape model exists!"
+        response += "Bored ape model exists! 2"
     else:
         response += "Bored ape model does NOT exist."
     return response
