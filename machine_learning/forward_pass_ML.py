16adcfa595b17db99bf3e81b4928c930c86f4503
Tom Mitcheson <tm21@curve10.doc.ic.ac.uk>
2022-03-24 15:16:48 +0000

    Trialling function for a forward pass from the website through a model and returning the price, the trait list, and the image link

---
+++ machine_learning/forward_pass_ML.py
@@ -0,0 +1,50 @@
+import pickle
+from pandas import DataFrame
+import pathlib
+import firebase_admin
+from firebase_admin import db
+
+cred_push_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_ML-prepped-database.json'
+cred_push = firebase_admin.credentials.Certificate(cred_push_key)
+default_app = firebase_admin.initialize_app(cred_push, {
+    'databaseURL':'https://ml-prepped-database-default-rtdb.europe-west1.firebasedatabase.app/'
+    })
+
+
+def find_price_predictor_from_tokenid(request):
+    cred_push = firebase_admin.credentials.Certificate(cred_push_key)
+    try:
+        firebase_admin.delete_app(transactions_app)
+    except:
+        a = cred_push # dummy operation
+    try:
+        ml_app = firebase_admin.initialize_app(cred_push, { 'databaseURL':'https://ml-prepped-database-default-rtdb.europe-west1.firebasedatabase.app/' } )
+    except:
+        a = cred_push # dummy operation
+    
+    # process request
+    collection_name = request['collection']
+    tokenID = request['tokenid']
+
+    # find model
+    filename = "../node_graph_data/" + collection_name + "_model.pkl"
+
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    # find input
+    nft_string = collection_name+tokenID
+    ref = db.reference(nft_string)
+    data_for_input = ref.get()
+
+    # format input 
+    data_for_input_json = DataFrame([data_for_input])
+    data_for_input_json = data_for_input_json.drop(['NameOfCollection', 'ethprice'], axis=1)
+    data_for_input_json['timestamp'] = 0
+
+    predicted_price = loaded_model.predict(data_for_input_json)
+
+    firebase_admin.delete_app(ml_app) # there will DEFINITELY be a better way of doing this!!
+
+
+    return predicted_price
+
