b516ffbe83702d0d9509edaf092073bc5203f10d
Patrick Moran <patrick.moran1998@yahoo.co.uk>
2022-05-15 17:49:45 +0100

    PLS GOD

--- machine_learning/graph_creation_two/create_list_of_whales.py
+++ machine_learning/graph_creation_two/create_list_of_whales.py
@@ -9,44 +9,22 @@ from firebase_admin import db
 # from trait_distribution import trait_distribution 
 import json
 import pathlib
-from collection_class_lite import Collection_lite
+import pickle
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+from retrieve_collections_from_pkl import retrieve_all_pickles_into_dict
 
 
-# CREATE LINK FOR 'FULL DATABASE'
-full_database_key = '/database_store_keys/key_for_full_database_store.json'
-all_tokens_key = '/database_store_keys/key_for_all_tokens_store.json'
-all_transactions_key = '/database_store_keys/key_for_all_transactions_store.json'
-cred_push_key = str(pathlib.Path(__file__).parent.resolve()) + full_database_key
-cred_push = firebase_admin.credentials.Certificate(cred_push_key)
-default_app = firebase_admin.initialize_app(cred_push, {
-    'databaseURL':'https://full-database-9c028-default-rtdb.europe-west1.firebasedatabase.app/'
-    })
-
-# CREATE LINK FOR 'ALL TRANSACTIONS' DATABASE
-
-cred_pull_transactions_key = str(pathlib.Path(__file__).parent.resolve()) + all_transactions_key
-cred_pull_transactions = firebase_admin.credentials.Certificate(cred_pull_transactions_key)
-transactions_app = firebase_admin.initialize_app(cred_pull_transactions, {
-    'databaseURL':'https://allcollections-6e66c-default-rtdb.europe-west1.firebasedatabase.app/'
-    }, name='transactions_app')
 
 
-# JUST ONE BORED APES EXAMPLE FOR NOW BUT CAN MAKE THIS SECTION TRAVERSABLE LATER
-ref = db.reference('/', app=transactions_app)
-
-def get_firebase_data_return_pkl_file(pkl_file_name, address):
-
-    
-    transactions = ref.order_by_child('contracthash').equal_to(address).get()
-    # print(transactions)
-    collection = Collection_lite(transactions)
-    collection.prep_data()
-    # print(collection)
-    collection.transactions_df.to_pickle(pkl_file_name)
-
-
-def create_node_graph_data(pkl_file_name, jsn_output_name):
-    transactions = pd.read_pickle(pkl_file_name)
+def create_node_graph_data(transactions, jsn_output_name):
+    # transactions = pd.read_pickle(pkl_file_name)
     transactions.sort_values("running_whale_weight", ascending = False, inplace = True)
 
     top_transactions = transactions['fromaddress'].unique()
@@ -117,31 +95,36 @@ def find_wallet_name(name):
 
 
 
-apeAddress = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
-# cryptoPunkMDAddress = '0x16F5A35647D6F03D5D3da7b35409D65ba03aF3B2'
-doodlesAddress = '0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e'
-coolCatsAddress = '0x1a92f7381b9f03921564a437210bb9396471050c'
-cryptoPunkAddress = '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB'
-cryptoKittiesAddress = '0x06012c8cf97bead5deae237070f9587f8e7a266d'
-cloneXAddress = '0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B'
-crypToadzAddress = '0x1CB1A5e65610AEFF2551A50f76a87a7d3fB649C6'
-boredApeKennelAddress = '0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623'
-pudgyPenguinAddress = '0xBd3531dA5CF5857e7CfAA92426877b022e612cf8'
+# apeAddress = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
+# # cryptoPunkMDAddress = '0x16F5A35647D6F03D5D3da7b35409D65ba03aF3B2'
+# doodlesAddress = '0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e'
+# coolCatsAddress = '0x1a92f7381b9f03921564a437210bb9396471050c'
+# cryptoPunkAddress = '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB'
+# cryptoKittiesAddress = '0x06012c8cf97bead5deae237070f9587f8e7a266d'
+# cloneXAddress = '0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B'
+# crypToadzAddress = '0x1CB1A5e65610AEFF2551A50f76a87a7d3fB649C6'
+# boredApeKennelAddress = '0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623'
+# pudgyPenguinAddress = '0xBd3531dA5CF5857e7CfAA92426877b022e612cf8'
 
-collection_addresses_dict = {'apeAddress': apeAddress, "doodlesAddress": doodlesAddress,
-        "coolCatsAddress": coolCatsAddress,
-        "cloneXAddress": cloneXAddress, "crypToadzAddress": crypToadzAddress,
-        "boredApeKennelAddress": boredApeKennelAddress, "pudgyPenguinAddress": pudgyPenguinAddress}
+# collection_addresses_dict = {'apeAddress': apeAddress, "doodlesAddress": doodlesAddress,
+#         "coolCatsAddress": coolCatsAddress,
+#         "cloneXAddress": cloneXAddress, "crypToadzAddress": crypToadzAddress,
+#         "boredApeKennelAddress": boredApeKennelAddress, "pudgyPenguinAddress": pudgyPenguinAddress}
 
-# Generate pkl and json files 
-list_pkl_filename = ['cool_cats.pkl', 'bored_ape.pkl', 'doodles.pkl', 'crypto_punks.pkl', 'cloneXAddress.pkl', 'cryptokitties.pkl', 'crypToadzAddress.pkl', 'boredApeKennelAddress.pkl', 'pudgyPenguinAddress.pkl']
-list_json_output_name = ['cool_cats.json', 'bored_ape_yacht_club.json', 'doodles.json', 'crypto_punks.json', 'cloneX.json', 'cryptokitties.json', 'cryptoadz.json', 'bored_ape_kennel_club.json', 'pudgy_penguins.json']
-list_address = [coolCatsAddress, apeAddress, doodlesAddress, cloneXAddress, cryptoKittiesAddress, crypToadzAddress, boredApeKennelAddress, pudgyPenguinAddress]
+# # Generate pkl and json files 
+# list_pkl_filename = ['cool_cats.pkl', 'bored_ape.pkl', 'doodles.pkl', 'crypto_punks.pkl', 'cloneXAddress.pkl', 'cryptokitties.pkl', 'crypToadzAddress.pkl', 'boredApeKennelAddress.pkl', 'pudgyPenguinAddress.pkl']
+#list_json_output_name = ['cool_cats.json', 'bored_ape_yacht_club.json', 'doodles.json', 'crypto_punks.json', 'cloneX.json', 'cryptokitties.json', 'cryptoadz.json', 'bored_ape_kennel_club.json', 'pudgy_penguins.json']
+# list_address = [coolCatsAddress, apeAddress, doodlesAddress, cloneXAddress, cryptoKittiesAddress, crypToadzAddress, boredApeKennelAddress, pudgyPenguinAddress]
 
-for i in range(len(list_pkl_filename)):
-    get_firebase_data_return_pkl_file(list_pkl_filename[i], list_address[i])
-    create_node_graph_data(list_pkl_filename[i], list_json_output_name[i])
-    print('Written', list_json_output_name[i])
+collection_dict = retrieve_all_pickles_into_dict()
+for name, collection in collection_dict.items():
+    transactions_df = collection.transactions_df
+    create_node_graph_data(transactions_df, name+ '.json')
+    print('Written', name)
+
+# for i in range(len(list_pkl_filename)):
+#     get_firebase_data_return_pkl_file(list_pkl_filename[i], list_address[i])
+    
 
 
 # #BORED APE DATA
